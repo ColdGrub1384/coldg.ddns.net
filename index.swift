66508fef6ff35fde8106cdf619e97502f651bdb7
Adrian Labbé <adrilabbelol@gmail.com>
2018-03-18 10:55:04 +0000

    Fix indentation

--- index.swift
+++ index.swift
@@ -74,7 +74,7 @@
 	print("background-position: center;")
 	print("}")
     
-    print(".container {")
-    print("background-color: rgba(0,0,0,0.5);")
-    print("}")
+	print(".container {")
+	print("background-color: rgba(0,0,0,0.5);")
+	print("}")
 }
