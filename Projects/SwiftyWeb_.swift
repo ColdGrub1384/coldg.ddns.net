0eba87356307149f8176ed83e6ae4f2c16311294
Adrian Labbé <adrilabbelol@gmail.com>
2018-08-08 18:14:23 -0400

    Fix indentation in SwiftyWeb_.swift

--- Projects/SwiftyWeb_.swift
+++ Projects/SwiftyWeb_.swift
@@ -2,21 +2,21 @@ import Foundation
 
 class SwiftyWeb: Project {
 
-	override var name: String {
-		return "SwiftyWeb"
-	}
+    override var name: String {
+        return "SwiftyWeb"
+    }
 
-	override var description: String {
-		return "Tool for doing CGIs with Swift."
-	}
+    override var description: String {
+ 	return "Tool for doing CGIs with Swift."
+    }
     
     override func details() {
         print("With SwiftyWeb, you can code HTML with a Swifty syntax and do server side things.<br/><br/>")
         "iframe".htm(["src='swiftyweb.html'", "width=90%", "height=90%", "frameborder=0"])
     }
 
-	override var urls: [URLForProject] {
-		return [URLForProject(title: "<img src='octocat.png'> GitHub", url: URL(string: "https://github.com/ColdGrub1384/SwiftyWeb")!)]
-	}
+    override var urls: [URLForProject] {
+        return [URLForProject(title: "<img src='octocat.png'> GitHub", url: URL(string: "https://github.com/ColdGrub1384/SwiftyWeb")!)]
+    }
 
 }
