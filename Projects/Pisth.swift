26dfacc6ca240b73a603bf7514cbb19385cf212b
Adrian Labbé <adrilabbelol@gmail.com>
2018-08-08 18:13:32 -0400

    Show Pisth as iOS and macOS app

--- Projects/Pisth.swift
+++ Projects/Pisth.swift
@@ -2,21 +2,21 @@ import Foundation
 
 class Pisth: Project {
 
-	override var name: String {
-		return "Pisth"
-	}
+    override var name: String {
+        return "Pisth"
+    }
 
-	override var description: String {
-		return "SSH and SFTP client for iOS."
-	}
+    override var description: String {
+        return "SSH and SFTP client for iOS and macOS."
+    }
 
-	override var urls: [URLForProject] {
-		return [URLForProject(title: " App Store", url: URL(string:"https://itunes.apple.com/cl/app/pisth/id1331070425?l=en&mt=8")!), URLForProject(title: "<img src='octocat.png' width=50%> GitHub", url: URL(string: "https://github.com/ColdGrub1384/Pisth")!)]
-	}
+    override var urls: [URLForProject] {
+        return [URLForProject(title: " App Store", url: URL(string:"https://itunes.apple.com/cl/app/pisth/id1331070425?l=en&mt=8")!), URLForProject(title: "<img src='octocat.png' width=50%> GitHub", url: URL(string: "https://github.com/ColdGrub1384/Pisth")!)]
+    }
     
     override func details() {
         
-        "iframe".htm(["src='https://pisth.github.io/ios'", "width=100%", "height=100%", "frameBorder=0", "style='border-radius:25px'"])
+        "iframe".htm(["src='https://pisth.github.io/'", "width=100%", "height=100%", "frameBorder=0", "style='border-radius:25px'"])
     }
 
 }
